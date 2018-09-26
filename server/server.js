f2621e321fa8720d3d407f3797164162a00e7818
unknown <jklfaeifncaslkfj>
2018-09-26 02:15:30 -0700

    a

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const express = require('express')
+
+const app = express()
+
+const staticDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(staticDirectoryPath))
+
+
+
+const port = proces.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`)
+})
